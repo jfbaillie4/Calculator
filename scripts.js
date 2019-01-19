3cf9638de8733234a4acde69e44bdc2452df9211
James Baillie <jfbaillie4@gmail.com>
2019-01-19 10:22:48 -0800

    add, subtract, divide, multiply working and called in order

--- scripts.js
+++ scripts.js
@@ -1,40 +1,100 @@
-function subtract (array) {
-	return array.reduce((total, value) => total - value, 0);
-};
-
-function sum (array) {
-	return (array.reduce((total, value) => parseInt(total) + parseInt(value), 0));
-};
-
 function multiply (array) {
 	return (array.length > 1 ? array.reduce((total, value) => total * value) : "Requires array.length > 2");
-};
-
-function divide (array) {
-    return (array.length > 1 ? array.reduce((total, value) => total / value) : "Requires array.length > 2");
 };
 
 function power(number, power) {
 	return number ** power;
 };
 
-function operate (numbers, operator) {
+//Everything below this line works, everything above this line doesn't
 
-}
+function operate (inputArray) {
+    for (i=0; i<inputArray.length; i++) {
+        if (inputArray[i]==="/") {
+            const division = divide(inputArray[i-1], inputArray[i+1]);
+            inputArray.splice(i-1,3,division);
+            i--;
+        }
+    };
+    for (i=0; i<inputArray.length; i++) {
+        if (inputArray[i]==="*") {
+            const multiple = multiply(inputArray[i-1], inputArray[i+1]);
+            inputArray.splice(i-1,3,multiple);
+            i--;
+        }
+    };
+    for (i=0; i<inputArray.length; i++) {
+        if (inputArray[i]==="+") {
+            const sum = add(inputArray[i-1], inputArray[i+1]);
+            inputArray.splice(i-1, 3, sum);
+            i--;
+        }
+    };
+    for (i=0; i<inputArray.length; i++) {
+        if (inputArray[i]==="-") {
+            const sub = subtract(inputArray[i-1], inputArray[i+1]);
+            inputArray.splice(i-1, 3, sub);
+            i--;
+        }
+    };
+        /*
+        if (inputArray[i]==="+") {
+            const sum = add(inputArray[i-1], inputArray[i+1]);
+            inputArray.splice(i-1, 3, sum);
+            i--;
+        } else if (inputArray[i]==="-") {
+            const sub = subtract(inputArray[i-1], inputArray[i+1]);
+            inputArray.splice(i-1, 3, sub);
+            i--;
+        } else {
+            console.log(i);
+        };*/
+    return inputArray;
+};
 
-const dipslayArray = [];
-const display = document.querySelector('.screen');
+function add (x, y) {
+	return parseInt(x) + parseInt(y);
+};
+
+function subtract (x, y) {
+	return parseInt(x) - parseInt(y);
+};
+
+function divide (x, y) {
+    return parseInt(x) / parseInt(y);
+};
+
+function multiply (x, y) {
+	return parseInt(x) * parseInt(y);
+};
+
+const integers = ["1","2","3","4","5","6","7","8","9","0"];
+const operators = ["+","-","*","/"]
+const displayArray = [];
+const keyRecordArray = [];
+const display = document.querySelector('.screen span');
 const buttons = Array.from(document.querySelectorAll('.button'));
 
+//This function now contains a lot of logic, might I want to break it out?
 function press (e) {
     const value = e.target.dataset.button;
-    if (value === "=") {
-        console.log("equals");
-        //display.innerHTML = sum(dipslayArray);
+    const lastvalue = displayArray[displayArray.length-1];
+    const lastentry = keyRecordArray[keyRecordArray.length-1];
+    keyRecordArray.push(value);
+    if (integers.includes(lastentry) && integers.includes(value)) {
+        const combinedvalue = lastvalue + value;
+        displayArray.splice(displayArray.length-1, 1, combinedvalue);
+        display.innerHTML = displayArray.join(""); 
+    } else if (value === "=") {
+        const result = operate(displayArray);
+        display.innerHTML = result.join(""); 
     } else {
-        dipslayArray.push(value);
-        display.innerHTML = dipslayArray.join(""); 
-    };
+        displayArray.push(value);
+        display.innerHTML = displayArray.join(""); 
+    }
 };
 
 buttons.forEach(button => button.addEventListener('click', press));
+
+
+//Question - do I need to be using the parse int or can the data- html tag pass integers
